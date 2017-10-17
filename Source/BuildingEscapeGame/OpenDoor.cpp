d4cbaaad930cb82633108d46510d46c850ec627c
AresV10 <31405933+AresV10@users.noreply.github.com>
2017-10-17 22:11:22 +0530

    Update OpenDoor.cpp

--- Source/BuildingEscapeGame/OpenDoor.cpp
+++ Source/BuildingEscapeGame/OpenDoor.cpp
@@ -1,7 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "Components/PrimitiveComponent.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -13,17 +18,17 @@ UOpenDoor::UOpenDoor()
 	// ...
 }
 
+ATriggerVolume* PressurePlate2;
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
-
-	// ...
-	
+	ActorTeleporting = GetWorld()->GetFirstPlayerController()->GetPawn();
+	Owner = GetOwner();
 }
+
 
 
 // Called every frame
@@ -31,7 +36,39 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-
-	// ...
+	if (GetTotalMassOfActorsOnTriggerPlate()> TriggerMass)
+	{
+		OnOpenRequest.Broadcast();
+	}
+	else
+	{
+		OnCloseRequest.Broadcast();
+	}
+	
+	if (PressurePlate2->IsOverlappingActor(ActorTeleporting))
+	{
+		TeleportActor();
+	}
+		
 }
 
+float UOpenDoor::GetTotalMassOfActorsOnTriggerPlate()
+{
+	float TotalMass = 0.f;
+	TArray<AActor*> OverlappingActors;
+	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
+	for (const auto* Actor : OverlappingActors)
+	{
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp, Warning, TEXT("%s on Pressure Plate"), *Actor->GetName())
+	}
+	return TotalMass;
+}
+
+void UOpenDoor::TeleportActor()
+{
+	//ActorTeleporting = GetWorld()->GetFirstPlayerController()->GetPawn();
+	ActorTeleporting ->SetActorLocation(FVector(1069.f, 113.f, 112.000687f));
+	UE_LOG(LogTemp, Warning, TEXT("%s on Pressure Plate And IT WORKSSSSSS"), *ActorTeleporting->GetName())
+
+}
